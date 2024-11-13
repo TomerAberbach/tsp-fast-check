28e0005dfd6b1dc2bc2ac73cd63dd91d9c7eabe1
Tomer Aberbach <tomer@stainlessapi.com>
2024-11-13 16:46:33 -0500

    gahhhh

--- src/emitter.ts
+++ src/emitter.ts
@@ -17,255 +17,286 @@ import {
   concat,
   entries,
   filter,
+  filterMap,
   flatMap,
+  index,
   join,
   map,
   pipe,
   reduce,
+  toArray,
+  toMap,
   toObject,
+  toSet,
   values,
 } from "lfi";
+import memoize from "memoize";
+import toposort from "toposort";
 
 export async function $onEmit(context: EmitContext) {
   if (context.program.compilerOptions.noEmit) {
     return;
   }
 
+  let arbitraryNamespace = convertNamespace(
+    context.program.getGlobalNamespaceType(),
+  );
+  // The global namespace will always have one user defined namespace, which should be the top-level namespace
+  // of the emitted JS file.
+  arbitraryNamespace = {
+    ...arbitraryNamespace.namespaces[0]!,
+    name: "",
+  };
+
   await emitFile(context.program, {
     path: resolvePath(context.emitterOutputDir, "output.js"),
-    content: pipe(
-      concat(
-        [`import * as fc from "fast-check";\n`],
-        pipe(
-          context.program.getGlobalNamespaceType().namespaces,
-          values,
-          filter((namespace) => namespace.name !== "TypeSpec"),
-          flatMap(namespaceTypes),
-          map(([name, type]) => `export const ${name} = ${emitType(type)};\n`),
-        ),
-      ),
-      join("\n"),
-    ),
+    content: `import * as fc from "fc"\n\n${stringifyNamespace(
+      arbitraryNamespace,
+      collectSharedArbitraries(arbitraryNamespace),
+    )}`,
   });
 }
 
-const emitType = (
-  type: Type,
-  decorators: DecoratorApplication[] = [],
-): string => {
-  switch (type.kind) {
-    case "Namespace":
-      return emitNamespace(type);
-    case "Model":
-      return emitModel(type);
-    case "Union":
-      return emitUnion(type);
-    case "Enum":
-      return emitEnum(type);
-    case "Scalar":
-      return emitScalar(type, decorators);
-  }
-
-  throw new Error(`Unhandled type: ${type.kind}`);
-};
-
-const emitNamespace = (namespace: Namespace): string =>
-  [
-    "{",
-    indent(
-      pipe(
-        namespaceTypes(namespace),
-        map(([name, type]) => `${name}: ${emitType(type)},`),
-        join("\n\n"),
-      ),
-    ),
-    "}",
-  ].join("\n");
-
-const namespaceTypes = (namespace: Namespace) =>
-  concat<[string, Type]>(
+const convertNamespace = (namespace: Namespace): ArbitraryNamespace => ({
+  name: namespace.name,
+  namespaces: pipe(
     namespace.namespaces,
-    namespace.models,
-    namespace.unions,
-    namespace.enums,
-    namespace.scalars,
+    values,
+    filter((namespace) => namespace.name !== "TypeSpec"),
+    map(convertNamespace),
+    reduce(toArray()),
+  ),
+  arbitraries: pipe(
+    concat<[string, Type]>(
+      namespace.models,
+      namespace.unions,
+      namespace.enums,
+      namespace.scalars,
+    ),
+    map(([name, type]): [string, Arbitrary] => [name, convertType(type)]),
+    reduce(toObject()),
+  ),
+});
+
+const convertType = memoize(
+  (type: Type, decorators: DecoratorApplication[] = []): Arbitrary => {
+    switch (type.kind) {
+      case "Model":
+        return convertModel(type);
+      case "Union":
+        return convertUnion(type);
+      case "Enum":
+        return convertEnum(type);
+      case "Scalar":
+        return convertScalar(type, decorators);
+    }
+
+    throw new Error(`Unhandled type: ${type.kind}`);
+  },
+);
+
+const convertModel = (model: Model): Arbitrary =>
+  createArbitrary((emitType) => {
+    const dictionary = model.indexer
+      ? model.indexer.key.name === "integer"
+        ? `fc.array(${emitType(model.indexer.value)})`
+        : `fc.dictionary(${emitType(model.indexer.key)}, ${emitType(model.indexer.value)})`
+      : null;
+    const record =
+      model.properties.size > 0
+        ? `fc.record(${emitOptions(
+            pipe(
+              model.properties,
+              map(([name, property]): [string, string] => [
+                name,
+                emitType(property.type, property.decorators),
+              ]),
+              reduce(toObject()),
+            ),
+            { emitEmpty: true },
+          )})`
+        : null;
+    if (dictionary && !record) {
+      return dictionary;
+    } else if (!dictionary && record) {
+      return record;
+    } else {
+      return [
+        "fc.tuple(",
+        indent(`${dictionary},`),
+        indent(`${record},`),
+        ").map(([dictionary, record]) => ({ ...dictionary, ...record }))",
+      ].join("\n");
+    }
+  });
+
+const convertUnion = (union: Union): Arbitrary =>
+  createArbitrary((emitType) =>
+    [
+      "fc.oneof(",
+      indent(
+        pipe(
+          union.variants,
+          map(([, variant]) => `${emitType(variant.type)},`),
+          join("\n"),
+        ),
+      ),
+      ")",
+    ].join("\n"),
   );
 
-const emitModel = (model: Model): string => {
-  const dictionary = model.indexer
-    ? model.indexer.key.name === "integer"
-      ? `fc.array(${emitType(model.indexer.value)})`
-      : `fc.dictionary(${emitType(model.indexer.key)}, ${emitType(model.indexer.value)})`
-    : null;
-  const record =
-    model.properties.size > 0
-      ? `fc.record(${emitOptions(
-          pipe(
-            model.properties,
-            map(([name, property]): [string, string] => [
-              name,
-              emitType(property.type, property.decorators),
-            ]),
-            reduce(toObject()),
-          ),
-          { emitEmpty: true },
-        )})`
-      : null;
-  if (dictionary && !record) {
-    return dictionary;
-  } else if (!dictionary && record) {
-    return record;
-  } else {
-    return [
-      "fc.tuple(",
-      indent(`${dictionary},`),
-      indent(`${record},`),
-      ").map(([dictionary, record]) => ({ ...dictionary, ...record }))",
-    ].join("\n");
-  }
-};
-
-const emitUnion = (union: Union): string =>
-  [
-    "fc.oneof(",
-    indent(
-      pipe(
-        union.variants,
-        map(([, variant]) => `${emitType(variant.type)},`),
-        join("\n"),
+const convertEnum = ($enum: Enum): Arbitrary =>
+  createArbitrary(
+    [
+      "fc.constantFrom(",
+      indent(
+        pipe(
+          $enum.members,
+          map(([, member]) => `${JSON.stringify(member.value)},`),
+          join("\n"),
+        ),
       ),
-    ),
-    ")",
-  ].join("\n");
+      ")",
+    ].join("\n"),
+  );
 
-const emitEnum = ($enum: Enum): string =>
-  [
-    "fc.constantFrom(",
-    indent(
-      pipe(
-        $enum.members,
-        map(([, member]) => `${JSON.stringify(member.value)},`),
-        join("\n"),
-      ),
-    ),
-    ")",
-  ].join("\n");
-
-const emitScalar = (
+const convertScalar = (
   scalar: Scalar,
   decorators: DecoratorApplication[] = [],
-): string => {
+): Arbitrary => {
   switch (scalar.name) {
     case "boolean":
-      return "fc.boolean()";
+      return createArbitrary("fc.boolean()");
     case "int8":
-      return emitInteger(scalar, decorators, { min: -128, max: 127 });
+      return convertInteger(scalar, decorators, { min: -128, max: 127 });
     case "int16":
-      return emitInteger(scalar, decorators, { min: -32_768, max: 32_767 });
+      return convertInteger(scalar, decorators, { min: -32_768, max: 32_767 });
     case "int32":
-      return emitInteger(scalar, decorators, {
+      return convertInteger(scalar, decorators, {
         min: -2_147_483_648,
         max: 2_147_483_647,
       });
     case "int64":
-      return emitBigInteger(scalar, decorators, {
+      return convertBigInteger(scalar, decorators, {
         min: -9_223_372_036_854_775_808n,
         max: 9_223_372_036_854_775_807n,
       });
     case "integer":
-      return emitBigInteger(scalar, decorators);
+      return convertBigInteger(scalar, decorators);
     case "float32":
-      return emitFloat(scalar, decorators);
+      return convertTypeFloat(scalar, decorators);
     case "float":
     case "float64":
     case "decimal":
     case "decimal128":
-      return emitDouble(scalar, decorators);
+      return convertDouble(scalar, decorators);
     case "string":
-      return emitString(scalar, decorators);
+      return convertString(scalar, decorators);
     case "bytes":
-      return "fc.int8Array()";
+      return createArbitrary("fc.int8Array()");
     case "url":
-      return "fc.webUrl()";
+      return createArbitrary("fc.webUrl()");
   }
 
   throw new Error(`Unhandled Scalar: ${scalar.name}`);
 };
 
-const emitInteger = (
+const convertInteger = (
   integer: Scalar,
   decorators: DecoratorApplication[],
   { min, max }: { min: number; max: number },
-) => {
+): Arbitrary => {
   const nameToDecorator = getNameToDecorator(
     concat(decorators, integer.decorators),
   );
-  return `fc.integer(${emitOptions({
-    min: getDecoratorValue(
-      Math.max(min, Number(nameToDecorator.$minValue?.[0] ?? min)),
-    ),
-    max: getDecoratorValue(
-      Math.min(max, Number(nameToDecorator.$maxValue?.[0] ?? max)),
-    ),
-  })})`;
+  return createArbitrary(
+    `fc.integer(${emitOptions({
+      min: getDecoratorValue(
+        Math.max(min, Number(nameToDecorator.$minValue?.[0] ?? min)),
+      ),
+      max: getDecoratorValue(
+        Math.min(max, Number(nameToDecorator.$maxValue?.[0] ?? max)),
+      ),
+    })})`,
+  );
 };
 
-const emitBigInteger = (
+const convertBigInteger = (
   integer: Scalar,
   decorators: DecoratorApplication[],
   { min, max }: { min?: bigint; max?: bigint } = {},
-) => {
+): Arbitrary => {
   const nameToDecorator = getNameToDecorator(
     concat(decorators, integer.decorators),
   );
-  return `fc.bigInt(${emitOptions({
-    min: getDecoratorValue(
-      min == null
-        ? null
-        : bigIntMax(min, BigInt(String(nameToDecorator.$minValue?.[0] ?? min))),
-    ),
-    max: getDecoratorValue(
-      max == null
-        ? null
-        : bigIntMin(max, BigInt(String(nameToDecorator.$maxValue?.[0] ?? max))),
-    ),
-  })})`;
+  return createArbitrary(
+    `fc.bigInt(${emitOptions({
+      min: getDecoratorValue(
+        min == null
+          ? null
+          : bigIntMax(
+              min,
+              BigInt(String(nameToDecorator.$minValue?.[0] ?? min)),
+            ),
+      ),
+      max: getDecoratorValue(
+        max == null
+          ? null
+          : bigIntMin(
+              max,
+              BigInt(String(nameToDecorator.$maxValue?.[0] ?? max)),
+            ),
+      ),
+    })})`,
+  );
 };
 
 const bigIntMax = (a: bigint, b: bigint) => (a > b ? a : b);
 const bigIntMin = (a: bigint, b: bigint) => (a < b ? a : b);
 
-const emitFloat = (float: Scalar, decorators: DecoratorApplication[]) => {
+const convertTypeFloat = (
+  float: Scalar,
+  decorators: DecoratorApplication[],
+): Arbitrary => {
   const nameToDecorator = getNameToDecorator(
     concat(decorators, float.decorators),
   );
-  return `fc.float(${emitOptions({
-    min: getDecoratorValue(nameToDecorator.$minValue?.[0]),
-    max: getDecoratorValue(nameToDecorator.$maxValue?.[0]),
-  })})`;
+  return createArbitrary(
+    `fc.float(${emitOptions({
+      min: getDecoratorValue(nameToDecorator.$minValue?.[0]),
+      max: getDecoratorValue(nameToDecorator.$maxValue?.[0]),
+    })})`,
+  );
 };
 
-const emitDouble = (double: Scalar, decorators: DecoratorApplication[]) => {
+const convertDouble = (
+  double: Scalar,
+  decorators: DecoratorApplication[],
+): Arbitrary => {
   const nameToDecorator = getNameToDecorator(
     concat(decorators, double.decorators),
   );
-  return `fc.double(${emitOptions({
-    min: getDecoratorValue(nameToDecorator.$minValue?.[0]),
-    max: getDecoratorValue(nameToDecorator.$maxValue?.[0]),
-  })})`;
+  return createArbitrary(
+    `fc.double(${emitOptions({
+      min: getDecoratorValue(nameToDecorator.$minValue?.[0]),
+      max: getDecoratorValue(nameToDecorator.$maxValue?.[0]),
+    })})`,
+  );
 };
 
-const emitString = (
+const convertString = (
   string: Scalar,
   decorators: DecoratorApplication[],
-): string => {
+): Arbitrary => {
   const nameToDecorator = getNameToDecorator(
     concat(decorators, string.decorators),
   );
-  return `fc.string(${emitOptions({
-    minLength: getDecoratorValue(nameToDecorator.$minLength?.[0]),
-    maxLength: getDecoratorValue(nameToDecorator.$maxLength?.[0]),
-  })})`;
+  return createArbitrary(
+    `fc.string(${emitOptions({
+      minLength: getDecoratorValue(nameToDecorator.$minLength?.[0]),
+      maxLength: getDecoratorValue(nameToDecorator.$maxLength?.[0]),
+    })})`,
+  );
 };
 
 const getNameToDecorator = (
@@ -316,3 +347,153 @@ const emitOptions = (
 };
 
 const indent = (code: string) => indentString(code, 2, { indent: " " });
+
+const createArbitrary = (
+  codeOrFn:
+    | string
+    | ((
+        emitType: (...args: Parameters<typeof convertType>) => string,
+      ) => string),
+): Arbitrary => {
+  if (typeof codeOrFn === "string") {
+    return { code: codeOrFn, placeholders: new Map() };
+  }
+
+  let index = 0;
+  const placeholders = new Map<string, Arbitrary>();
+  return {
+    code: codeOrFn((type) => {
+      const placeholder = `$${index++}`;
+      placeholders.set(placeholder, convertType(type));
+      return placeholder;
+    }),
+    placeholders,
+  };
+};
+
+const collectSharedArbitraries = (
+  namespace: ArbitraryNamespace,
+): Map<Arbitrary, string> => {
+  const sharedArbitraries = new Set<Arbitrary>();
+  const countArbitraryReferences = (namespace: ArbitraryNamespace) => {
+    for (const nestedNamespace of namespace.namespaces) {
+      countArbitraryReferences(nestedNamespace);
+    }
+    for (const arbitrary of Object.values(namespace.arbitraries)) {
+      sharedArbitraries.add(arbitrary);
+    }
+  };
+  countArbitraryReferences(namespace);
+
+  const sharedArbitraryDependencyGraph = pipe(
+    sharedArbitraries,
+    flatMap(
+      (arbitrary): Iterable<[Arbitrary, Arbitrary | undefined]> =>
+        arbitrary.placeholders.size === 0
+          ? [[arbitrary, undefined]]
+          : pipe(
+              arbitrary.placeholders,
+              values,
+              map((dependency): [Arbitrary, Arbitrary] => [
+                arbitrary,
+                dependency,
+              ]),
+            ),
+    ),
+    reduce(toArray()),
+  );
+  return pipe(
+    toposort(sharedArbitraryDependencyGraph).reverse(),
+    filter((arbitrary) => sharedArbitraries.has(arbitrary)),
+    index,
+    map(([index, arbitrary]): [Arbitrary, string] => [
+      arbitrary,
+      `shared${index}`,
+    ]),
+    reduce(toMap()),
+  );
+};
+
+const stringifyNamespace = (
+  namespace: ArbitraryNamespace,
+  sharedArbitraries: Map<Arbitrary, string>,
+): string => {
+  if (!namespace.name) {
+    return stringifyTopLevelNamespace(namespace, sharedArbitraries);
+  }
+
+  return [
+    "{",
+    indent(
+      pipe(
+        concat(
+          map(
+            (namespace) =>
+              `${namespace.name}: ${stringifyNamespace(namespace, sharedArbitraries)},`,
+            namespace.namespaces,
+          ),
+          map(
+            ([name, arbitrary]) =>
+              `${name}: ${stringifyArbitrary(arbitrary, sharedArbitraries)},`,
+            entries(namespace.arbitraries),
+          ),
+        ),
+        join("\n\n"),
+      ),
+    ),
+    "}",
+  ].join("\n");
+};
+
+const stringifyTopLevelNamespace = (
+  namespace: ArbitraryNamespace,
+  sharedArbitraries: Map<Arbitrary, string>,
+): string =>
+  pipe(
+    concat(
+      map(
+        ([arbitrary, name]) =>
+          `export const ${name} = ${stringifyArbitraryDefinition(arbitrary, sharedArbitraries)};\n`,
+        entries(sharedArbitraries),
+      ),
+      map(
+        (namespace) =>
+          `export const ${namespace.name} = ${stringifyNamespace(namespace, sharedArbitraries)};\n`,
+        namespace.namespaces,
+      ),
+      map(
+        ([name, arbitrary]) =>
+          `export const ${name} = ${stringifyArbitrary(arbitrary, sharedArbitraries)};\n`,
+        entries(namespace.arbitraries),
+      ),
+    ),
+    join("\n\n"),
+  );
+
+type ArbitraryNamespace = {
+  name: string;
+  namespaces: ArbitraryNamespace[];
+  arbitraries: Record<string, Arbitrary>;
+};
+
+const stringifyArbitrary = (
+  arbitrary: Arbitrary,
+  sharedArbitraries: Map<Arbitrary, string>,
+): string =>
+  sharedArbitraries.get(arbitrary) ??
+  stringifyArbitraryDefinition(arbitrary, sharedArbitraries);
+
+const stringifyArbitraryDefinition = (
+  arbitrary: Arbitrary,
+  sharedArbitraries: Map<Arbitrary, string>,
+): string =>
+  [...arbitrary.placeholders].reduce(
+    (code, [name, placeholder]) =>
+      code.replace(name, stringifyArbitrary(placeholder, sharedArbitraries)),
+    arbitrary.code,
+  );
+
+type Arbitrary = {
+  code: string;
+  placeholders: Map<string, Arbitrary>;
+};
--- tsp-output/tsp-fast-check/output.js
+++ tsp-output/tsp-fast-check/output.js
@@ -1,77 +1,12 @@
-import * as fc from "fast-check";
+import * as fc from "fc";
 
-export const Pets = {
-  Toys: {
-    Toy: fc.record({
-      name: fc.string(),
-    }),
-  },
-
-  Pet: fc.record({
-    breed: fc.oneof(
-      fc.record({
-        int64: fc.bigInt({
-          min: -9223372036854775808n,
-          max: 9223372036854775807n,
-        }),
-        integer: fc.bigInt(),
-        decimal: fc.double(),
-        url: fc.webUrl(),
-        bytes: fc.int8Array(),
-        name: fc.string(),
-        name2: fc.array(fc.string()),
-        age: fc.integer({
-          min: -2147483648,
-          max: 2147483647,
-        }),
-        blah: fc.boolean(),
-        blah2: fc.float(),
-        blah3: fc.double(),
-        blah4: fc.dictionary(fc.string(), fc.string()),
-      }),
-      fc.record({
-        name2: fc.string(),
-        age: fc.integer({
-          min: -2147483648,
-          max: 2147483647,
-        }),
-        age2: fc.integer({
-          min: -32768,
-          max: 32767,
-        }),
-      }),
-      fc
-        .tuple(
-          fc.dictionary(fc.string(), fc.string()),
-          fc.record({
-            name3: fc.string(),
-          }),
-        )
-        .map(([dictionary, record]) => ({ ...dictionary, ...record })),
-    ),
-    id: fc.integer({
-      min: -2147483648,
-      max: 2147483647,
-    }),
-    name: fc.string({
-      minLength: 1,
-      maxLength: 4,
-    }),
-    age: fc.integer({
-      min: 0,
-      max: 100,
-    }),
-    kind: fc.constantFrom("dog", "cat", "fish", "bird", "reptile"),
-  }),
-
-  petType: fc.constantFrom("dog", "cat", "fish", "bird", "reptile"),
-};
-
-export const Blah = fc.record({
+export const shared0 = fc.record({
   name: fc.string(),
 });
 
-export const Breed = fc.oneof(
+export const shared1 = fc.constantFrom("dog", "cat", "fish", "bird", "reptile");
+
+export const shared2 = fc.oneof(
   fc.record({
     int64: fc.bigInt({
       min: -9223372036854775808n,
@@ -112,3 +47,35 @@ export const Breed = fc.oneof(
     )
     .map(([dictionary, record]) => ({ ...dictionary, ...record })),
 );
+
+export const shared3 = fc.record({
+  breed: shared2,
+  id: fc.integer({
+    min: -2147483648,
+    max: 2147483647,
+  }),
+  name: fc.string(),
+  age: fc.integer({
+    min: -2147483648,
+    max: 2147483647,
+  }),
+  kind: shared1,
+});
+
+export const shared4 = fc.record({
+  name: fc.string(),
+});
+
+export const Pets = {
+  Toys: {
+    Toy: shared4,
+  },
+
+  Pet: shared3,
+
+  petType: shared1,
+};
+
+export const Blah = shared0;
+
+export const Breed = shared2;
