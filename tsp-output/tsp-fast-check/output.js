ec7402e54660d5f2ad13c01c0e39605beca00795
Tomer Aberbach <tomer@stainlessapi.com>
2024-11-13 11:40:25 -0500

    chore: commit output

---
+++ tsp-output/tsp-fast-check/output.js
@@ -0,0 +1,66 @@
+import * as fc from "fast-check";
+
+export const Pets = {
+  Toys: {
+    Toy: fc.record({
+      name: fc.string(),
+    }),
+  },
+
+  Pet: fc.record({
+    breed: fc.oneof(
+      fc.record({
+        name: fc.string(),
+        name2: fc.array(fc.string()),
+        age: fc.integer({
+          min: -2147483648,
+          max: 2147483647,
+        }),
+        blah: fc.boolean(),
+        blah2: fc.float(),
+        blah3: fc.double(),
+        blah4: fc.dictionary(fc.string(), fc.string()),
+      }),
+      fc.record({
+        name2: fc.string(),
+        age: fc.integer({
+          min: -2147483648,
+          max: 2147483647,
+        }),
+        age2: fc.integer({
+          min: -32768,
+          max: 32767,
+        }),
+      }),
+      fc.tuple(
+        fc.dictionary(fc.string(), fc.string()),
+        fc.record({
+          name3: fc.string(),
+        }),
+      ).map(([dictionary, record]) => ({ ...dictionary, ...record })),
+    ),
+    id: fc.integer({
+      min: -2147483648,
+      max: 2147483647,
+    }),
+    name: fc.string({
+      minLength: 1,
+      maxLength: 4,
+    }),
+    age: fc.integer({
+      min: 0,
+      max: 100,
+    }),
+    kind: fc.constantFrom(
+      "dog",
+      "cat",
+      "fish",
+      "bird",
+      "reptile",
+    ),
+  }),
+};
+
+export const Blah = fc.record({
+  name: fc.string(),
+});
